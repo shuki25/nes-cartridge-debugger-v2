8f8d4fb35539aabfa2c60d01e1dbf03d082aef04
dr. josh butler <31372342+shuki25@users.noreply.github.com>
2025-12-16 00:54:42 -0500

    Update splash.c

--- Core/Src/splash.c
+++ Core/Src/splash.c
@@ -20,10 +20,10 @@ void splash(void) {
 	ssd1306_Fill(Black);
 	ssd1306_SetCursor(16, 1);
 	ssd1306_WriteString("BUTLER", Font_16x26, White);
-	ssd1306_SetCursor(42, 28);
-	ssd1306_WriteString("TECH", Font_11x18, White);
+	ssd1306_SetCursor(47, 28);
+	ssd1306_WriteString("TEK", Font_11x18, White);
 	ssd1306_SetCursor(0, 53);
-	ssd1306_WriteString("(C)2024", Font_7x10, White);
+	ssd1306_WriteString("(C)2025", Font_7x10, White);
 	snprintf((char*) buffer, 12, "v%d.%d.%d%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, BOARD_REVISION);
 	uint8_t len = strlen((char*) buffer);
 	ssd1306_SetCursor(128 - (len * 7), 53);
